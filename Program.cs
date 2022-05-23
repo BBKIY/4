3f931dc151ed8fa8b8b244de35df10328d22858a
BBKIY <104115351+BBKIY@users.noreply.github.com>
2022-05-23 23:44:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureCount = 52;
+            int perLineCount = 3;
+            int rowFilled = pictureCount / perLineCount;
+            int overFlowCount = pictureCount % perLineCount;
+            Console.WriteLine("Полностью заполненных рядов " + rowFilled);
+            Console.WriteLine("Картинок сверх меры " + overFlowCount);
+        }
+    }
+}
